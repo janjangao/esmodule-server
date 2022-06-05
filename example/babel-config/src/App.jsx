1f2986da33eed06301f4f7c84fff0d8289c219ac
jian.gao <jian.gao@shopee.com>
2022-06-06 00:42:40 +0800

    fix: add hash for dynamic import to reload

--- example/babel-config/src/App.jsx
+++ example/babel-config/src/App.jsx
@@ -1,6 +1,5 @@
 import React from "https://esm.sh/react@18.1.0";
 import Content from "./Content.tsx";
-// import Content from "./Content.jsx";
 
 export default function App({ content }) {
   return <Content text={content} />;
--- example/babel-config/src/Content.jsx
+++
@@ -1,6 +0,0 @@
-import React from "https://esm.sh/react@18.1.0";
-
-
-export default function ({ text }) {
-  return <div>{text}</div>;
-}
